2357028c3516b5ae39eb8b37533a67358e8c6746
Vishal Yadav <yadavvi91@gmail.com>
2019-01-03 01:00:43 +0530

    Test that creating a superuser creates a user with is_superuser and is_staff permissions
    NOTE: The test fails when we run -
    docker-compose run app sh -c "python manage.py test && flake8"

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,13 @@ class ModelTests(TestCase):
                 None,
                 'test123'
             )
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
